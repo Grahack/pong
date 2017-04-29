559c454af563a4cb63ab2082b7ecc105220ef6a8
Christophe Gragnic <cgragnic@protonmail.com>
2017-04-29 20:37:27 +0200

    Améliorations debray doucet

--- 2016/debray.js
+++ 2016/debray.js
@@ -8,17 +8,21 @@ var ball_h;
 var ball_v;
 var raquette1_h;
 var raquette1_v;
- vitesse_h = 3;
- vitesse_v = 3;
- canvas_h = 640;
- canvas_v = 480;
- ball_h = 40;
- ball_v = 40;
- raquette1_h = 10;
- raquette1_v = 50;
+var score1;
+vitesse_h = 3;
+vitesse_v = 3;
+canvas_h = 640;
+canvas_v = 480;
+ball_h = 20;
+ball_v = 20;
+raquette1_h = 10;
+raquette1_v = 50;
+raquette1_ecart = 60;
+score1 = 0;
 
 
 function setup() {
+  frameRate(60);
   createCanvas(canvas_h, canvas_v);
   background(0,0,0);
   fill(255,255,255);
@@ -27,17 +31,33 @@ function setup() {
 }
 
 function draw() {
+	clear();
+	background(0,0,0);
 	//balle
   ellipse(x, y, ball_h, ball_v);
-if ((x >= canvas_h - ball_h/2) || (x <= ball_h/2)) {
+if ((x >= canvas_h - ball_h/2) || (x <= ball_h/2)){
   vitesse_h = -vitesse_h;
 }
-if ((y >= canvas_v - ball_h/2) || (y <= ball_v/2)) {
+if ((y >= canvas_v - ball_v/2) || (y <= ball_v/2)) {
   vitesse_v = -vitesse_v;
+}
+if (((y + ball_v/2 >= mouseY - raquette1_h/2) && (y - ball_v/2 <= mouseY + raquette1_h/2) && (x + ball_h/2 >= raquette1_ecart) && (x - ball_h/2 <= raquette1_ecart + raquette1_h))){
+  vitesse_h = -vitesse_h;
+  score1 = score1 + 1;
+  vitesse_h = vitesse_h + 0.5;
+  vitesse_v = vitesse_v + 0.5;
+}
+if (x <= raquette1_ecart + raquette1_h - 2){
+  document.write("GAME OVER \nPress F5 to restart \nYou scored : " + score1);
+  x = canvas_h/2;
+  y = canvas_v/2;
+  vitesse_h = 0;
+  vitesse_v = 0;
 }
   x = x + vitesse_h;
   y = y + vitesse_v;
-  
+
+
 	//raquette
-	rect(50, mouseY, raquette1_h, raquette1_v)
+	rect(raquette1_ecart, mouseY - raquette1_v / 2, raquette1_h, raquette1_v);
 }
--- 2016/doucet.js
+++ 2016/doucet.js
@@ -1,44 +1,70 @@
 var x;
 var y;
 var R;
-var vitesse_h;
-var vitesse_v;
+var a;
+var boty;
+
+
+var vitesse_x;
+var vitesse_y;
 
 function setup() {
   createCanvas(640, 480);
-  background(0,0,255);
-  fill(255,0,0);
+  background(0,0,0);
+  fill(255,255,255);
   x = 320;
   y = 240;
-  vitesse_h = 5;
-  vitesse_v = -5;
-  R = 5
+  vitesse_x = 5;
+  vitesse_y = -5;
+  R = 5;
+  a = 20;
+  
  
 }
 
 function draw() {
-background(0,0,255);
-  ellipse(x, y, 40, 40);
-  if (x >= 640) {
-      vitesse_h = -5;
+	
+ 
+	
+  
+  background(0,0,0);
+  rect(315,0,10,640)
+  rect(R, mouseY , 10,60);
+  ellipse(x, y, 30, 30);
+  if (x >= 630) {
+      vitesse_x = 0;
   }
   if (y >= 480) {
-      vitesse_v = -5;
+      vitesse_y = -5;
   }
   if (x <= R) {
-      vitesse_h = 0;
+      vitesse_x = 0;
+	  vitesse_y = 0;
+	 
+	  
   }
   if (y <= 0) {
-      vitesse_v = 5;
+      vitesse_y = 5;
   }
-  x = x + vitesse_h;
-  y = y + vitesse_v;
+  if ( x<=30 && y<=mouseY+60 && y>=mouseY) {
+	  
+  vitesse_x = 5 
+  
+  }
+  if ( x>=630 && y<=boty+60 && y>=boty) {
+	  
+  vitesse_x = -5;
+  
+  }
+ 
+  boty=y-30;
+  rect(630,boty,10,60)
+  x = x + vitesse_x;
+  y = y + vitesse_y;
 
-rect(R, mouseY , 10,60)
-
-if (x == R+40 && y <= mouseY+30 && y >= mouseY-30) {
-vitesse_h = 5 
-}
+  }
 
 
-}
+
+
+
